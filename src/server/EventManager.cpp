5cbed0521fd903abcecd5d71767f33271248b660
Valeria <70722334+Hikabu@users.noreply.github.com>
2024-09-10 02:26:21 +0700

    small mistakes

--- src/server/EventManager.cpp
+++ src/server/EventManager.cpp
@@ -11,18 +11,18 @@ EventManager::EventManager() : maxSocket(0)
 }
 EventManager::~EventManager(){}
 
-void EventManager::addServerSocket(ServerSocket &serverSocket)
+void EventManager::addServerSocket(ServerSocket &serverSocket) //adds the fd of the new server socket to the master
 {
-    serverSockets.push_back(serverSocket);
+    serverSockets.push_back(serverSocket); //and evman can keep track of all the server sockets it's managing
 
-    FD_SET(serverSocket.getListenSocket(), &read_master);
+    FD_SET(serverSocket.getListenSocket(), &read_master); // should remember
     if (serverSocket.getListenSocket() > maxSocket)
     {
         maxSocket = serverSocket.getListenSocket();
     }
 }
 
-void EventManager::CreateAddClientSocket(ServerSocket &Socket)
+void EventManager::CreateAddClientSocket(ServerSocket &Socket)//accepting a new client connection and setting it up for non-blocking io
 {
     struct sockaddr_in clientAddr;
     int serverSocket = Socket.getListenSocket();
@@ -33,7 +33,12 @@ void EventManager::CreateAddClientSocket(ServerSocket &Socket)
         perror ("Error accepting connection");
         return;
     }
-    fcntl (clientSocket, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
+    /* O_NONBLOCK read(), write(), send(), recv()) on that socket will not block indefinitely
+    for select this flag handle mulyiple connrctions withoit getting stuck waing on any single client
+    FD_CLOEXEC marks the fd to be cloded automaticslly when the procss calls exec() to run a new program
+    and smt about  child processes in future to not inherit
+    */
+    fcntl (clientSocket, F_SETFL, O_NONBLOCK, FD_CLOEXEC); // need to change(read more about flags)
     FD_SET(clientSocket, &read_master);
 
     if (clientSocket > maxSocket)
@@ -47,20 +52,21 @@ void EventManager::CreateAddClientSocket(ServerSocket &Socket)
     (void)clientAddr;
 }
 
-void EventManager::waitandleEvents()
+void EventManager::waitandleEvents() //handle multiple client connections
 {
     while (maxSocket)
     {
-        readSet = read_master;
+        readSet = read_master; //keep all fd
         writeSet = write_master;
-        int activity = select(maxSocket + 1, &readSet, &writeSet, NULL, NULL);
+        /*wait for activity on multiple sockets without consuming CPU resources.*/
+        int activity = select(maxSocket + 1, &readSet, &writeSet, NULL, NULL); //monitor multiple fd, waiting fd become ready for some I/O operation
         if (activity < 0)
         {
             perror("Error in select");
             continue;
         }
         for(size_t i = 0; i < serverSockets.size(); i++) {
-            if (FD_ISSET(serverSockets[i].getListenSocket(), &readSet)){
+            if (FD_ISSET(serverSockets[i].getListenSocket(), &readSet)){ //  if any of the server sockets are ready to accept a new connection
                 CreateAddClientSocket(serverSockets[i]);
             }
         }
@@ -69,9 +75,9 @@ void EventManager::waitandleEvents()
             int currentSocket = current.getClientSocket();
             char buffer[3000];
             memset(buffer, 0, 3000);
-            if (FD_ISSET(currentSocket, &readSet)) {
-                int bytesRead = recv(currentSocket, buffer, 3000, 0);
-                if (bytesRead == -1) {
+            if (FD_ISSET(currentSocket, &readSet)) {//Checks if a fd is set in a given set
+                int bytesRead = recv(currentSocket, buffer, 3000, 0); //Receives data from a socket
+                if (bytesRead == -1) { //not shure of structure
                     delete (*it);
                     it = clientSockets.erase(it);
                     --it;
@@ -85,7 +91,7 @@ void EventManager::waitandleEvents()
                     //some important request stuff
                 }
             }
-            if (FD_ISSET(currentSocket, &writeSet)) {
+            if (FD_ISSET(currentSocket, &writeSet)) { //sending data to clients that are ready to receive.
                 int byteWrite = 1024;
                 (void) byteWrite;
                 std::cout << "Wow im here\n";
@@ -99,7 +105,7 @@ void EventManager::waitandleEvents()
                 // std::cout << response << std::endl;
                 // //std::cout << response.toString() << std::endl;
                 // std::string responseStr = response.toString();
-                // send(currentSocket, responseStr.c_str(), responseStr.length(), 0);
+                // send(currentSocket, responseStr.c_str(), responseStr.length(), 0);// Sends data on a socket.
                 // std::cout << "------------------HTML content sent-------------------" << std::endl;
                 // close(new_socket);
 		    }
